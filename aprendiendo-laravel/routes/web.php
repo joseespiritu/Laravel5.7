34820caa96067b3e46b0fd983b7450cdde147e29
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-06 07:14:18 -0500

    Rutas basicas

--- aprendiendo-laravel/routes/web.php
+++ aprendiendo-laravel/routes/web.php
@@ -12,5 +12,18 @@
 */
 
 Route::get('/', function () {
+    echo "<h1>Hola Mundo</h1>"; 
     return view('welcome');
+});
+
+//GET: Conseguir datos
+//POST: Guardar datos
+//PUT: Actualizar recursos
+//DELETE: Eliminar recursos
+
+Route::get('/mostrarFecha',function(){
+    $titulo = "Estoy mostrando la fecha";
+    return view('mostrar-fecha', array(
+        'titulo' => $titulo,
+    ));
 });
