db9f90649321a78f7393576d50c143af548788b5
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-06 19:38:28 -0500

    Parametros opcionales, condicion where en la ruta

--- aprendiendo-laravel/routes/web.php
+++ aprendiendo-laravel/routes/web.php
@@ -28,8 +28,12 @@ Route::get('/mostrarFecha',function(){
     ));
 });
 
-Route::get('/pelicula/{titulo?}',function($titulo = 'No hay una pelicula seleccionada'){
+Route::get('/pelicula/{titulo}/{year?}',function($titulo = 'No hay una pelicula seleccionada', $year = 2019){
    return view('pelicula', array(
       'titulo' => $titulo,
+       'year' => $year,
    ));
-});
+})->where(array(
+    'titulo' => '[a-zA-Z]+',
+    'year' => '[0-9]+',
+));
