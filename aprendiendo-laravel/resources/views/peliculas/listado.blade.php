3ebfc2b9bb3a5e1542b039dd2c230f63c0c30641
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-07 07:17:51 -0500

    Motor Blade, primer uso

--- aprendiendo-laravel/resources/views/peliculas/listado.blade.php
+++ aprendiendo-laravel/resources/views/peliculas/listado.blade.php
@@ -1,2 +1,13 @@
-<h1><?=$titulo?></h1>
-<h2><?=$listado[2]?></h2>
+<h1>{{$titulo}}</h1>
+<h2>{{$listado[2]}}</h2>
+
+<p>{{ date('Y') }}</p>
+
+<!--Esto es un comentario html-->
+<?php 
+//    Esto es un comentario php
+?>
+
+{{--Esto es un comentario blade--}}
+
+{{ $director or 'No hay ningun director' }}
