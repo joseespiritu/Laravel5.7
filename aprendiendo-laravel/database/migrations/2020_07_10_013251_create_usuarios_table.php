34e76a76ceb1f6a0d4128aabb11af32b04b493f6
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-10 06:59:58 -0500

    Migraciones SQL

--- aprendiendo-laravel/database/migrations/2020_07_10_013251_create_usuarios_table.php
+++ aprendiendo-laravel/database/migrations/2020_07_10_013251_create_usuarios_table.php
@@ -13,14 +13,23 @@ class CreateUsuariosTable extends Migration
      */
     public function up()
     {
-        Schema::create('usuarios', function (Blueprint $table) {
+        /* Schema::create('usuarios', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre', 255);
             $table->string('email', 255);
             $table->string('password', 255);
             $table->integer('edad');
             $table->timestamps();
-        });
+        }); */
+
+        DB::statement("
+            CREATE TABLE usuarios(
+            id int(255) auto_increment not null,
+            nombre varchar(255),
+            email varchar(255),
+            password varchar(255),
+            PRIMARY KEY(id)
+        );");
     }
 
     /**
