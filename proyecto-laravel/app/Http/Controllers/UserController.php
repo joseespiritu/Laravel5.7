4c420b2e5c63fcd3997d794a9092976972323f52
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-17 06:53:03 -0500

    Recibir datos del formulario

--- proyecto-laravel/app/Http/Controllers/UserController.php
+++ proyecto-laravel/app/Http/Controllers/UserController.php
@@ -9,4 +9,18 @@ class UserController extends Controller
     public function config(){
         return view('user.config');
     }
+
+    public function update(Request $request){
+        $id = \Auth::user()->id;
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        $nick = $request->input('nick');
+        $email = $request->input('email');
+
+        var_dump($id);
+        var_dump($name);
+        var_dump($surname);
+        die();
+
+    }
 }
