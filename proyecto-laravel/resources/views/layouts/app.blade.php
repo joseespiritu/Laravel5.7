c1c5ed511fa7ebca8c40a80d5cc2faaa281e0cbc
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-18 19:33:28 -0500

    Formulario para crear imagenes

--- proyecto-laravel/resources/views/layouts/app.blade.php
+++ proyecto-laravel/resources/views/layouts/app.blade.php
@@ -50,10 +50,10 @@
                             </li>
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Inicio</a>
+                                <a class="nav-link" href="{{ route('home') }}">Inicio</a>
                             </li>
                             <li>
-                                <a class="nav-link" href="#">Subir Imagen</a>
+                                <a class="nav-link" href="{{ route('image.create') }}">Subir Imagen</a>
                             </li>
                             <li>
                                 @include('includes.avatar')
