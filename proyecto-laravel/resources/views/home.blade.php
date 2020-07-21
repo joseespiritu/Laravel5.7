0ae5db854bc9f679267a4763158d1ec03a7d8618
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-20 20:28:40 -0500

    Listado de imagenes

--- proyecto-laravel/resources/views/home.blade.php
+++ proyecto-laravel/resources/views/home.blade.php
@@ -6,19 +6,27 @@
         <div class="col-md-8">
 
             @include('includes.message')
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            @foreach($images as $image)
+                <div class="card pub_image">
+                    <div class="card-header">
+
+                        @if($image->user->image)
+                        <div class="container-avatar">
+                            <img src="{{ route('user.avatar',['filename' => $image->user->image]) }}" alt="Imagen Usuario" class="avatar"/>
                         </div>
-                    @endif
+                        @endif
 
-                    You are logged in!
+                        <div class="data-user">
+                            {{$image->user->name.' '.$image->user->surname.' | @'.$image->user->nick}}
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
