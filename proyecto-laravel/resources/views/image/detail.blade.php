1d4b18c490928077e590e292dad82a59b217c37b
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-22 22:39:00 -0500

    Formulario de comentarios

--- proyecto-laravel/resources/views/image/detail.blade.php
+++ proyecto-laravel/resources/views/image/detail.blade.php
@@ -7,7 +7,7 @@
 
             @include('includes.message')
 
-                <div class="card pub_image">
+                <div class="card pub_image pub_image_detail">
                     <div class="card-header">
 
                         @if($image->user->image)
@@ -23,7 +23,7 @@
                     </div>
 
                     <div class="card-body">
-                        <div class="image-container">
+                        <div class="image-container image-detail">
                             <img src="{{ route('image.file',['filename'=> $image->image_path])}}" alt="Image"/>
                         </div>
     
@@ -37,10 +37,22 @@
                         <div class="likes">
                             <img src="{{ asset('img/heart-black.png')}}" alt="heart"/>
                         </div>
-                        <div class="comments">
-                            <a href="" class="btn btn-sm btn-warning btn-comments">
-                                Comentarios ({{count($image->comments)}})
-                            </a>
+                        <div class="clearfix">
+                            <div class="comments">
+                                <h2>Comentarios ({{count($image->comments)}})</h2>
+                                <hr>
+
+                                <form action="" method="POST">
+                                    @csrf
+                                    <input type="hidden" name="image_id" value="{{}$image->id}"/>
+                                    <p>
+                                        <textarea class="form-control" name="content" required></textarea>
+                                    </p>
+                                    <button type="submit" class="btn btn-success">
+                                        Enviar
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
