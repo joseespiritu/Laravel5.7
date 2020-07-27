4c3b4156973c920b98b256e25cc16d05e47ea2b5
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-26 22:22:07 -0500

    Maquetacion del perfil

--- proyecto-laravel/resources/views/user/profile.blade.php
+++ proyecto-laravel/resources/views/user/profile.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
-            <div class="data-user">
+            <div class="profile-user">
                 @if($user->image)
                 <div class="container-avatar">
                     <img src="{{ route('user.avatar',['filename' => $user->image]) }}" alt="Imagen Usuario" class="avatar" />
@@ -17,7 +17,12 @@
                     <h2>{{$user->name . ' ' . $user->surname}}</h2>
                     <p>{{'Se unió: Hace '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
                 </div>
+
+                <div class="clearfix"></div>
+                <hr>
             </div>
+
+            <div class="clearfix"></div>
 
             @foreach($user->images as $image)
             @include('includes.image',['image' => $image])
