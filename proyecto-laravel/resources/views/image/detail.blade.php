4de6756d5df8ff0317437bae4084e3539eda7e2d
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-27 20:43:24 -0500

    Modal Bootstrap 4 Eliminar

--- proyecto-laravel/resources/views/image/detail.blade.php
+++ proyecto-laravel/resources/views/image/detail.blade.php
@@ -7,100 +7,130 @@
 
             @include('includes.message')
 
-                <div class="card pub_image pub_image_detail">
-                    <div class="card-header">
+            <div class="card pub_image pub_image_detail">
+                <div class="card-header">
 
-                        @if($image->user->image)
-                        <div class="container-avatar">
-                            <img src="{{ route('user.avatar',['filename' => $image->user->image]) }}" alt="Imagen Usuario" class="avatar"/>
-                        </div>
-                        @endif
+                    @if($image->user->image)
+                    <div class="container-avatar">
+                        <img src="{{ route('user.avatar',['filename' => $image->user->image]) }}" alt="Imagen Usuario" class="avatar" />
+                    </div>
+                    @endif
 
-                        <div class="data-user">
-                            {{$image->user->name.' '.$image->user->surname}}
-                            <span class="nickname">{{' | @'.$image->user->nick}}</span>
-                        </div>
+                    <div class="data-user">
+                        {{$image->user->name.' '.$image->user->surname}}
+                        <span class="nickname">{{' | @'.$image->user->nick}}</span>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <div class="image-container image-detail">
+                        <img src="{{ route('image.file',['filename'=> $image->image_path])}}" alt="Image" />
                     </div>
 
-                    <div class="card-body">
-                        <div class="image-container image-detail">
-                            <img src="{{ route('image.file',['filename'=> $image->image_path])}}" alt="Image"/>
-                        </div>
-    
-                        <div class="description">
-                            <span class="nickname">{{ '@'.$image->user->nick }}</span>
-                            <span class="nickname date"> {{' | '.\FormatTime::LongTimeFilter($image->created_at)}}  </span>            
+                    <div class="description">
+                        <span class="nickname">{{ '@'.$image->user->nick }}</span>
+                        <span class="nickname date"> {{' | '.\FormatTime::LongTimeFilter($image->created_at)}} </span>
                         <p>
                             {{ $image->description }}
                         </p>
-                        </div>
+                    </div>
 
-                        <div class="likes">  
+                    <div class="likes">
 
-                            <!-- Comprobar si el usuario le dio like a la imagen -->
-                            <?php $user_like = false; ?>
-                            @foreach($image->likes as $like)
-                                @if($like->user->id == Auth::user()->id)
-                                    <?php $user_like = true; ?>
-                                @endif
-                            @endforeach
-
-                            @if($user_like)
-                                <img src="{{ asset('img/heart-red.png')}}" data-id="{{$image->id}}" alt="heart" class="btn-dislike"/>
-                            @else
-                                <img src="{{ asset('img/heart-black.png')}}" data-id="{{$image->id}}" alt="heart" class="btn-like"/>
-                            @endif
-                            <span class="number_likes">{{count($image->likes)}}</span>
-                        </div>
-
-                        @if(Auth::user() && Auth::user()->id == $image->user->id)
-                        <div class="actions">
-                            <a href="" class="btn btn-sm btn-primary">Actualizar</a>
-                            <a href="{{ route('image.delete', ['id' => $image->id]) }}" class="btn btn-sm btn-danger">Borrar</a>
-                        </div>
+                        <!-- Comprobar si el usuario le dio like a la imagen -->
+                        <?php $user_like = false; ?>
+                        @foreach($image->likes as $like)
+                        @if($like->user->id == Auth::user()->id)
+                        <?php $user_like = true; ?>
                         @endif
+                        @endforeach
 
-                        <div class="clearfix"></div>
-                            <div class="comments">
-                                <h2>Comentarios ({{count($image->comments)}})</h2>
-                                <hr>
+                        @if($user_like)
+                        <img src="{{ asset('img/heart-red.png')}}" data-id="{{$image->id}}" alt="heart" class="btn-dislike" />
+                        @else
+                        <img src="{{ asset('img/heart-black.png')}}" data-id="{{$image->id}}" alt="heart" class="btn-like" />
+                        @endif
+                        <span class="number_likes">{{count($image->likes)}}</span>
+                    </div>
 
-                                <form action="{{ route('comment.save')}}" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="image_id" value="{{$image->id}}"/>
-                                    <p>
-                                        <textarea class="form-control {{$errors->has('content') ? 'is-invalid' : ''}}" name="content" required></textarea>
-                                        @if($errors->has('content'))
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('content') }}</strong>
-                                            </span>
-                                        @endif
-                                    </p>
-                                    <button type="submit" class="btn btn-success">
-                                        Enviar
-                                    </button>
-                                </form>
+                    @if(Auth::user() && Auth::user()->id == $image->user->id)
+                    <div class="actions">
+                        <a href="" class="btn btn-sm btn-primary">Actualizar</a>
+                        
+                        <!-- Button to Open the Modal -->
+                        <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#myModal">
+                            Eliminar
+                        </button>
 
-                                <hr>
-                                @foreach($image->comments as $comment)
-                                    <div class="comment">
-                                        <span class="nickname">{{ '@'.$comment->user->nick }}</span>
-                                        <span class="nickname date"> {{' | '.\FormatTime::LongTimeFilter($comment->created_at)}}  </span>            
-                                        <p>
-                                            {{ $comment->content }}
-                                            <br>
-                                            @if(Auth::check() && ($comment->user_id == Auth::user()->id || $comment->image->user_id == Auth::user()->id))
-                                            <a href="{{ route('comment.delete', ['id' => $comment->id]) }}" class="btn btn-sm btn-danger">
-                                                Eliminar
-                                            </a>
-                                            @endif
-                                        </p>
+                        <!-- The Modal -->
+                        <div class="modal" id="myModal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
 
+                                    <!-- Modal Header -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">¿Estas seguro?</h4>
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
                                     </div>
-                                @endforeach
+
+                                    <!-- Modal body -->
+                                    <div class="modal-body">
+                                        Si eliminas esta imagen nunca podrás recuperarla, ¿Estas seguro de querer borrarla?
+                                    </div>
+
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
+                                        <a href="{{ route('image.delete', ['id' => $image->id]) }}" class="btn btn-danger">Borrar definitivamente</a>
+                                    </div>
+
+                                </div>
                             </div>
+                        </div>
+                    </div>
+                    @endif
+
+                    <div class="clearfix"></div>
+                    <div class="comments">
+                        <h2>Comentarios ({{count($image->comments)}})</h2>
+                        <hr>
+
+                        <form action="{{ route('comment.save')}}" method="POST">
+                            @csrf
+                            <input type="hidden" name="image_id" value="{{$image->id}}" />
+                            <p>
+                                <textarea class="form-control {{$errors->has('content') ? 'is-invalid' : ''}}" name="content" required></textarea>
+                                @if($errors->has('content'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('content') }}</strong>
+                                </span>
+                                @endif
+                            </p>
+                            <button type="submit" class="btn btn-success">
+                                Enviar
+                            </button>
+                        </form>
+
+                        <hr>
+                        @foreach($image->comments as $comment)
+                        <div class="comment">
+                            <span class="nickname">{{ '@'.$comment->user->nick }}</span>
+                            <span class="nickname date"> {{' | '.\FormatTime::LongTimeFilter($comment->created_at)}} </span>
+                            <p>
+                                {{ $comment->content }}
+                                <br>
+                                @if(Auth::check() && ($comment->user_id == Auth::user()->id || $comment->image->user_id == Auth::user()->id))
+                                <a href="{{ route('comment.delete', ['id' => $comment->id]) }}" class="btn btn-sm btn-danger">
+                                    Eliminar
+                                </a>
+                                @endif
+                            </p>
+
+                        </div>
+                        @endforeach
                     </div>
                 </div>
+            </div>
 
         </div>
     </div>
