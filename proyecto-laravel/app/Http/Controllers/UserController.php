be4a8057686869bb9f217008309d2efaa5ca531b
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-28 20:11:05 -0500

    Metodo del buscador

--- proyecto-laravel/app/Http/Controllers/UserController.php
+++ proyecto-laravel/app/Http/Controllers/UserController.php
@@ -15,8 +15,16 @@ class UserController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(){
-        $users = User::orderBy('id', 'desc')->paginate(5);
+    public function index($search = null){
+        if(!empty($search)){
+            $users = User::where('nick','LIKE','%'.$search.'%')
+                                ->orWhere('name', 'LIKE', '%'.$search.'%')
+                                ->orWhere('surname', 'LIKE', '%'.$search.'%')
+                                ->orderBy('id', 'desc')
+                                ->paginate(5);
+        } else {
+            $users = User::orderBy('id', 'desc')->paginate(5);
+        }
         return view('user.index', [
             'users' => $users
         ]);
--- proyecto-laravel/routes/web.php
+++ proyecto-laravel/routes/web.php
@@ -38,27 +38,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/* GENERALES */
 Auth::routes();
-
 Route::get('/', 'HomeController@index')->name('home');
+
+/* USUARIO */
 Route::get('/configuracion','UserController@config')->name('config');
 Route::post('user/update', 'UserController@update')->name('user.update');
 Route::get('user/avatar/{filename}', 'UserController@getImage')->name('user.avatar');
+Route::get('/perfil/{id}','UserController@profile')->name('profile');
+Route::get('/personas/{search?}', 'UserController@index')->name('user.index');
 
+/* IMAGEN */
 Route::get('/subir-imagen','ImageController@create')->name('image.create');
 Route::post('/image/save','ImageController@save')->name('image.save');
 Route::get('/image/file/{filename}', 'ImageController@getImage')->name('image.file');
 Route::get('/imagen/{id}', 'ImageController@detail')->name('image.detail');
-Route::post('/comment/save','CommentController@save')->name('comment.save');
-Route::get('/comment/delete/{id}', 'CommentController@delete')->name('comment.delete');
-Route::get('/like/{image_id}','LikeController@like')->name('like.save');
-Route::get('/dislike/{image_id}','LikeController@dislike')->name('like.delete');
-
-Route::get('/likes','LikeController@index')->name('likes');
-
-Route::get('/perfil/{id}','UserController@profile')->name('profile');
 Route::get('/image/delete/{id}', 'ImageController@delete')->name('image.delete');
 Route::get('/imagen/editar/{id}', 'ImageController@edit')->name('image.edit');
 Route::post('/image/update','ImageController@update')->name('image.update');
 
-Route::get('/personas', 'UserController@index')->name('user.index');
+/* COMENTARIO */
+Route::post('/comment/save','CommentController@save')->name('comment.save');
+Route::get('/comment/delete/{id}', 'CommentController@delete')->name('comment.delete');
+
+/* LIKE */
+Route::get('/like/{image_id}','LikeController@like')->name('like.save');
+Route::get('/dislike/{image_id}','LikeController@dislike')->name('like.delete');
+Route::get('/likes','LikeController@index')->name('likes');
+
+
+
